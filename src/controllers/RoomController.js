1f04c86b3b35073411cee516c9e7e07658e7166c
Raphael Lima <raphaeldesousalm@gmail.com>
2021-09-22 18:54:31 -0300

    Criação da tabela no banco de dados e inserção das perguntas

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,26 @@
+const Database = require('../db/config')
+
 module.exports = {
-  create(req, res) {
-    let roomid = 1234
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+
+    let roomid = ''
+
+    for (let i = 0; i < 6; i++) {
+      roomid += Math.floor(Math.random() * 10).toString()
+    }
+
+    db.run(`
+    INSERT INTO rooms (
+      id,
+      pass
+    ) VALUES (
+      ${parseInt(roomid)},
+      ${pass}
+    )`)
+
+    db.close()
 
     res.redirect(`/room/${roomid}`)
   }
