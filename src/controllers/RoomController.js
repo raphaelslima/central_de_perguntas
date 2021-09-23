cdee5b388d241bace32af235b98534a801a244cc
Raphael Lima <raphaeldesousalm@gmail.com>
2021-09-23 11:59:26 -0300

    Inserção de perguntas no db

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -7,21 +7,36 @@ module.exports = {
 
     let roomid = ''
 
-    for (let i = 0; i < 6; i++) {
-      roomid += Math.floor(Math.random() * 10).toString()
-    }
+    let isRoom = true
 
-    db.run(`
-    INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomid)},
-      ${pass}
-    )`)
+    while (isRoom) {
+      /*CRIA O ID DA SALA*/
+      for (let i = 0; i < 6; i++) {
+        roomid += Math.floor(Math.random() * 10).toString()
+      }
+      /* VERIFICA SE EXISTE */
+      const roomExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomExistIds.some(roomExistId => roomExistId === roomid)
+
+      /*INSERE NA SALA*/
+      if (!roomExistIds) {
+        db.run(`
+        INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomid)},
+          ${pass}
+        )`)
+      }
+    }
 
     db.close()
 
     res.redirect(`/room/${roomid}`)
+  },
+  open(req, res) {
+    const roomId = req.params.room
+    res.render('room', { roomId: roomId })
   }
 }
--- src/route.js
+++ src/route.js
@@ -4,13 +4,18 @@ const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
+/* ROTAS PARA O LAYOUT */
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
-route.get('/room/:room', (req, res) => res.render('room'))
 
-route.post('/question/:room/:question/:action', QuestionController.index)
+/* ROTAS DE SALA */
+route.get('/room/:room', RoomController.open)
 route.post('/create-room', RoomController.create)
+
+/* ROTAS DE PERGUNTAS */
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
